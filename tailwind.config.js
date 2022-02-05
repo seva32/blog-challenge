25eb984610e86ae9de168cef18881f7a633dbf8b
Sebas <sebas.warsaw@gmail.com>
2022-02-05 09:52:15 -0300

    adds tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      ...colors,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
