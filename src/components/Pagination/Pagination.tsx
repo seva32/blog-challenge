7653a0d4df6dcfead9ef51388db4c8795dce363d
Sebas <sebas.warsaw@gmail.com>
2022-02-06 15:08:43 -0300

    Fix pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -50,28 +50,11 @@ class Pagination extends Component<PaginationProps, State> {
     this.state = { currentPage: 1 };
   }
 
-  // ...
-
-  /**
-   * Let's say we have 10 pages and we set pageNeighbours to 2
-   * Given that the current page is 6
-   * The pagination control will look like the following:
-   *
-   * (1) < {4 5} [6] {7 8} > (10)
-   *
-   * (x) => terminal pages: first and last page(always visible)
-   * [x] => represents current page
-   * {...x} => represents page neighbours
-   */
   fetchPageNumbers = () => {
     const totalPages = this.totalPages;
     const currentPage = this.state.currentPage;
     const pageNeighbours = this.pageNeighbours;
 
-    /**
-     * totalNumbers: the total page numbers to show on the control
-     * totalBlocks: totalNumbers + 2 to cover for the left(<) and right(>) controls
-     */
     const totalNumbers = this.pageNeighbours * 2 + 3;
     const totalBlocks = totalNumbers + 2;
 
@@ -80,11 +63,6 @@ class Pagination extends Component<PaginationProps, State> {
       const endPage = Math.min(totalPages - 1, currentPage + pageNeighbours);
       let pages = range(startPage, endPage) as any;
 
-      /**
-       * hasLeftSpill: has hidden pages to the left
-       * hasRightSpill: has hidden pages to the right
-       * spillOffset: number of hidden pages either to the left or to the right
-       */
       const hasLeftSpill = startPage > 2;
       const hasRightSpill = totalPages - endPage > 1;
       const spillOffset = totalNumbers - (pages.length + 1);
